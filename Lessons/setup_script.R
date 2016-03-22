4c2f3313e809b4f0385a6f4e6950cba86bfdc64b
Philippe Marchand <pmarchand@rstudio01.research.sesync.org>
2016-03-22 14:31:10 -0400

    Add setup script

---
+++ Lessons/setup_script.R
@@ -0,0 +1,35 @@
+# Function to install packages if they are not already in library
+install_missing <- function(pkgs, repos) {
+    missing_pkgs <- setdiff(pkgs, rownames(installed.packages()))
+    if (length(missing_pkgs) > 0) install.packages(missing_pkgs, repos = repos)
+}
+
+# Install packages required for the Spring 2016 Computational Institute
+pkg_list <- c("tidyr", "dplyr", "RSQLite", "sp", "rgdal", "rgeos",
+              "raster", "shiny")
+
+install_missing(pkg_list, repos = "http://cran.us.r-project.org")
+
+
+# Verify that packages can be loaded
+suppressMessages({
+    library(tidyr)
+    library(dplyr)
+    library(RSQLite)
+    library(sp)
+    library(rgdal)
+    library(rgeos)
+    library(raster)
+    library(shiny)    
+})
+
+
+# Check access to files
+species <- read.csv("/nfs/public-data/ci-spring2016/Data/species.csv")
+
+# If all the code above ran without any "Warning" or "Error", 
+# you are ready for the workshop!
+
+
+
+
